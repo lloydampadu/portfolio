fc2698f3874f2d46028f24918fa955065e12a5a2
lloydampadu <63337298+lloydampadu@users.noreply.github.com>
2026-04-22 08:12:50 +0000

    fix next config typescript error

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-<<<<<<< HEAD
   typescript: {
-    ignoreBuildErrors: true,
-=======
-  eslint: {
-    ignoreDuringBuilds: true, // Ignore ESLint errors and warnings during builds
-  },
-  typescript: {
-    ignoreBuildErrors: true, // Ignores type errors during builds
->>>>>>> d9a8fcb28aa01cc1a71bfec332e0550747bb66ae
-  },
+    ignoreBuildErrors: true
+  }
 };
 
 export default nextConfig;
