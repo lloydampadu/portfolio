93effd0672d60af4f3529149b09d730f5702c9c9
lloydampadu <63337298+lloydampadu@users.noreply.github.com>
2026-04-22 08:29:37 +0000

    revert to before conflict fixes

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+<<<<<<< HEAD
   typescript: {
-    ignoreBuildErrors: true
-  }
+    ignoreBuildErrors: true,
+=======
+  eslint: {
+    ignoreDuringBuilds: true, // Ignore ESLint errors and warnings during builds
+  },
+  typescript: {
+    ignoreBuildErrors: true, // Ignores type errors during builds
+>>>>>>> d9a8fcb28aa01cc1a71bfec332e0550747bb66ae
+  },
 };
 
 export default nextConfig;
